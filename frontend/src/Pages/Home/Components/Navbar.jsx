20dac95476610e8de57ef1022595c3516f15b094
Rishabh Kumar <97455703+rk26072003@users.noreply.github.com>
2023-02-12 08:53:40 +0530

    fixed navbar

--- frontend/src/Pages/Home/Components/Navbar.jsx
+++ frontend/src/Pages/Home/Components/Navbar.jsx
@@ -48,7 +48,8 @@ function Navbar() {
       <a class="mr-5 hover:text-gray-900 cursor-pointer">Experiences</a>
     </nav>
 
-    <button onClick={handleClick} class=" text-gray-700 inline-flex items-center bg-green-400 border-0 py-1 px-3 focus:outline-none hover:bg-green-200 rounded text-base mt-4 md:mt-0">Login
+    <button onClick={handleClick} class=" text-gray-700 inline-flex items-center bg-green-400 border-0 py-1 px-3 focus:outline-none hover:bg-green-200 rounded text-base mt-4 md:mt-0">
+      <a href="/dashboard/admin"> Login </a>
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
